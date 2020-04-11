dc58eb9d1d88d2adc6c44b19500ba868b19a9d9b
aleksey <ilkivv@yandex.ru>
2020-04-11 18:37:47 +0700

    Создание меню, формирование макета

---
+++ database/migrations/2020_04_11_110435_create_menus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_id')->unsigned()->nullable()->index();
+            $table->string('name');
+            $table->string('url');
+            $table->string('uri');
+            $table->tinyInteger('sort_order')->default(0);
+            $table->boolean('active')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
